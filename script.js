7a497e4a88a5295a0915db7e0136a1a58d5b6fd6
José Antonio Cano <canodelacuadra@gmail.com>
2017-03-09 13:23:39 +0100

    crear funcion y boton en HTML

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//agrupamos la funcion pares y nones
+function paresynones(){
+
 document.write('vamos a jugar a pares y nones. El jugador 1 siempre elige nones y el jugador 2 siempre elige pares <br>');
 var tiradaJugador = prompt('Jugador 1 dinos un numero del 1 al 5');
 document.write('El jugador 1 ha sacado = '+ tiradaJugador + '<br>');
@@ -13,3 +16,4 @@ if(suma % 2 === 0){
    document.write('El jugador 1 ha ganado <br>'); 
 }
 
+}//fin de la funcion pares y nones
